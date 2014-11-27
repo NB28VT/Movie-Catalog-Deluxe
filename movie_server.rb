96b2b2a9170cc2f27aaf89694fb33cd7640d7318
Nathan Burgess <nathan.burgess28@hotmail.com>
2014-11-27 08:49:17 -0500

    DB queries santitized.

--- movie_server.rb
+++ movie_server.rb
@@ -25,7 +25,7 @@ get '/movie_catalog/actors' do
   query = "SELECT actors.name, actors.id FROM actors ORDER BY actors.name;"
 
   db_connection do |connection|
-    @actors = connection.exec(query)
+    @actors = connection.exec_params(query, data)
   end
 
   erb :actors
@@ -34,15 +34,14 @@ end
 get '/movie_catalog/actors/:id' do
   @id = params[:id]
 
-  # NOT PROTECTED FOR SQL INJECTION
   actors_movies_query = "SELECT movies.title, actors.name,
-  cast_members.character, movies.id AS movie_id FROM movies JOIN cast_members
+  cast_members.character, movies.id AS movie_id FROM movies     JOIN cast_members
   ON movies.id = cast_members.movie_id JOIN actors
   ON actors.id = cast_members.actor_id
-  WHERE actors.id = #{@id};"
+  WHERE actors.id = $1;"
 
   db_connection do |connection|
-    @actor_info = connection.exec(actors_movies_query)
+    @actor_info = connection.exec(actors_movies_query, [@id])
   end
 
   @actor_info.to_a.each do |actor|
@@ -73,30 +72,28 @@ end
 
 get '/movie_catalog/movies/:id' do
   @id = params[:id]
-  # BREAK QUERIES START WITH MOVIE INFO
 
   query = "SELECT movies.title, genres.name AS genre, studios.name as studio
   FROM movies
   JOIN studios ON movies.studio_id = studios.id
   LEFT OUTER JOIN genres ON movies.genre_id = genres.id
-  WHERE movies.id = #{@id};"
+  WHERE movies.id = $1;"
 
   cast_query = "SELECT actors.name AS name, cast_members.character AS character,
   actors.id AS id
   FROM actors
   JOIN cast_members ON cast_members.actor_id = actors.id
   JOIN movies ON cast_members.movie_id = movies.id
-  WHERE movies.id = #{@id};"
+  WHERE movies.id = $1;"
 
   db_connection do |connection|
-    @movie_data = connection.exec(query)
+    @movie_data = connection.exec_params(query, [@id])
   end
 
 
 
-# NEED TO ADD THIS TO VIEWS
   db_connection do |connection|
-    @cast_data = connection.exec(cast_query)
+    @cast_data = connection.exec_params(cast_query, [@id])
   end
 
   erb :movie_info
