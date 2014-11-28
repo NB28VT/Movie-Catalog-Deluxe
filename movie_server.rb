2afa14904257a2bddb0d669df69cdadd367658c6
Nathan Burgess <nathan.burgess28@hotmail.com>
2014-11-28 11:51:31 -0500

    Functioning order by rating, title, year links

--- movie_server.rb
+++ movie_server.rb
@@ -54,24 +54,7 @@ end
 
 
 get '/movie_catalog/movies' do
-  query = "SELECT movies.title, movies.id AS id,
-  movies.year, movies.rating,
-  studios.name AS studio, genres.name AS genre
-  FROM movies
-  JOIN studios ON movies.studio_id = studios.id
-  LEFT OUTER JOIN genres ON movies.genre_id = genres.id
-  order BY movies.title;"
-
-  db_connection do |connection|
-    @movies = connection.exec(query)
-  end
-  erb :movies
-end
-
-# REFACTOR TO HANDLE MULTIPLE REQUESTS
-
-get '/movie_catalog/sorted_by_year' do
-
+  @order = params[:order]
 
 
   query = "SELECT movies.title, movies.id AS id,
@@ -80,30 +63,11 @@ get '/movie_catalog/sorted_by_year' do
   FROM movies
   JOIN studios ON movies.studio_id = studios.id
   LEFT OUTER JOIN genres ON movies.genre_id = genres.id
-  ORDER BY movies.year;"
+  order BY movies.#{@order};"
 
   db_connection do |connection|
     @movies = connection.exec(query)
   end
-
-  erb :movies
-end
-
-# Order by RATING NOT WORKING
-
-get '/movie_catalog/sorted_by_rating' do
-  query = "SELECT movies.title, movies.id AS id,
-  movies.year, movies.rating,
-  studios.name AS studio, genres.name AS genre
-  FROM movies
-  JOIN studios ON movies.studio_id = studios.id
-  LEFT OUTER JOIN genres ON movies.genre_id = genres.id
-  ORDER BY movies.rating;"
-
-  db_connection do |connection|
-    @movies = connection.exec(query)
-  end
-
   erb :movies
 end
 
